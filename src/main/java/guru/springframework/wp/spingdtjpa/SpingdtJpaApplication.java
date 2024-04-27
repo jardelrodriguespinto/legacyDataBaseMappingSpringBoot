54f86ba92645d98c9de6943e1860a982ffd2ddcf
jardelrodriguespinto <ghmagno910@gmail.com>
2024-04-27 12:19:51 -0300

    First commit

---
+++ src/main/java/guru/springframework/wp/spingdtjpa/SpingdtJpaApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.wp.spingdtjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingdtJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingdtJpaApplication.class, args);
+	}
+
+}
